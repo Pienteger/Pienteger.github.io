b913fe63c965049ef9bd1ebfeb4af25ea1205d9f
MD-Hasibul_Hasan <hasibulofcs@gmail.com>
2023-03-11 13:06:20 +0600

    marketplace ui implemented

--- src/Pages/Marketplace.tsx
+++ src/Pages/Marketplace.tsx
@@ -16,7 +16,9 @@ import style from "../Pages/Css/MarketPlace.module.css";
 const Marketplace = () => {
   return (
     <div className="d-flex flex-column flex-wrap">
-      <div className={`market_header bg-dark ${style["minheight7rem"]} p-5`}>
+      <div
+        className={`market_header ${style["carbon_effect_bg"]} ${style["minheight7rem"]} px-0 py-5`}
+      >
         <h1 className="text-white text-center">Find a Solution</h1>
         <div className="d-flex align-items-center justify-content-center">
           <form className="bg-white rounded d-flex align-items-center py-2 px-3 my-4">
@@ -29,48 +31,175 @@ const Marketplace = () => {
           </form>
         </div>
       </div>
-      <div className="container m-0 px-0 py-5 d-flex">
-        <div className="container m-0 px-0 sidebar">
-          <h3 className="text-dark">Solution Categories</h3>
-          <ul className="list-unstyled">
-            <li>item 1</li>
-            <li>item 2</li>
-            <li>item 3</li>
-            <li>item 4</li>
-            <li>item 5</li>
-            <li>item 6</li>
-            <li>item 7</li>
-            <li>item 8</li>
-            <li>item 9</li>
-            <li>item 10</li>
-          </ul>
-        </div>
-        <div className="container m-0 px-0 content_cards">
-          <Card style={{ maxWidth: "25vw" }}>
-            <CardHeader
-              header={<img src={openai_logo} style={{ maxHeight: 50 }} />}
-            ></CardHeader>
-            <p>
-              OpenAi is a company that is working on artificial intelligence and
-              machine learning.
-            </p>
-            <CardFooter>
-              <Button
-                as="a"
-                href="/marketplace/openai"
-                icon={<Info20Regular />}
+      <div className="container m-0 p-0 d-flex">
+        <div className="row w-100 mx-0">
+          <div className="container mx-0 px-0 col-3 sidebar">
+            <h3 className="text-dark pb-4">Solution Categories</h3>
+            <ul className="list-unstyled d-flex flex-column pe-5">
+              <li
+                className={`${style["border_bottom"]} py-3 ${style["font1_1"]}`}
               >
-                Learn More
-              </Button>
-              <Button
-                as="a"
-                href="/marketplace/openai_pricing"
-                icon={<Money20Regular />}
+                item 1
+              </li>
+              <li
+                className={`${style["border_bottom"]} py-3 ${style["font1_1"]}`}
               >
-                Pricing
-              </Button>
-            </CardFooter>
-          </Card>
+                item 2
+              </li>
+              <li
+                className={`${style["border_bottom"]} py-3 ${style["font1_1"]}`}
+              >
+                item 3
+              </li>
+              <li
+                className={`${style["border_bottom"]} py-3 ${style["font1_1"]}`}
+              >
+                item 4
+              </li>
+              <li
+                className={`${style["border_bottom"]} py-3 ${style["font1_1"]}`}
+              >
+                item 5
+              </li>
+              <li
+                className={`${style["border_bottom"]} py-3 ${style["font1_1"]}`}
+              >
+                item 6
+              </li>
+              <li
+                className={`${style["border_bottom"]} py-3 ${style["font1_1"]}`}
+              >
+                item 7
+              </li>
+              <li
+                className={`${style["border_bottom"]} py-3 ${style["font1_1"]}`}
+              >
+                item 8
+              </li>
+              <li
+                className={`${style["border_bottom"]} py-3 ${style["font1_1"]}`}
+              >
+                item 9
+              </li>
+              <li
+                className={`${style["border_bottom"]} py-3 ${style["font1_1"]}`}
+              >
+                item 10
+              </li>
+            </ul>
+          </div>
+          <div className="container mx-0 px-0 col-9 content_cards">
+            <div className="row mx-0 px-0">
+              <div className="col-12 col-md-6 col-lg-4 mx-0 px-2 pb-4">
+                <Card style={{ width: "100%" }}>
+                  <CardHeader
+                    header={<img src={openai_logo} style={{ maxHeight: 50 }} />}
+                  ></CardHeader>
+                  <p>
+                    OpenAi is a company that is working on artificial
+                    intelligence and machine learning.
+                  </p>
+                  <CardFooter>
+                    <Button
+                      as="a"
+                      href="/marketplace/openai"
+                      icon={<Info20Regular />}
+                    >
+                      Learn More
+                    </Button>
+                    <Button
+                      as="a"
+                      href="/marketplace/openai_pricing"
+                      icon={<Money20Regular />}
+                    >
+                      Pricing
+                    </Button>
+                  </CardFooter>
+                </Card>
+              </div>
+              <div className="col-12 col-md-6 col-lg-4 mx-0 px-2 pb-4">
+                <Card style={{ width: "100%" }}>
+                  <CardHeader
+                    header={<img src={openai_logo} style={{ maxHeight: 50 }} />}
+                  ></CardHeader>
+                  <p>
+                    OpenAi is a company that is working on artificial
+                    intelligence and machine learning.
+                  </p>
+                  <CardFooter>
+                    <Button
+                      as="a"
+                      href="/marketplace/openai"
+                      icon={<Info20Regular />}
+                    >
+                      Learn More
+                    </Button>
+                    <Button
+                      as="a"
+                      href="/marketplace/openai_pricing"
+                      icon={<Money20Regular />}
+                    >
+                      Pricing
+                    </Button>
+                  </CardFooter>
+                </Card>
+              </div>
+              <div className="col-12 col-md-6 col-lg-4 mx-0 px-2 pb-4">
+                <Card style={{ width: "100%" }}>
+                  <CardHeader
+                    header={<img src={openai_logo} style={{ maxHeight: 50 }} />}
+                  ></CardHeader>
+                  <p>
+                    OpenAi is a company that is working on artificial
+                    intelligence and machine learning.
+                  </p>
+                  <CardFooter>
+                    <Button
+                      as="a"
+                      href="/marketplace/openai"
+                      icon={<Info20Regular />}
+                    >
+                      Learn More
+                    </Button>
+                    <Button
+                      as="a"
+                      href="/marketplace/openai_pricing"
+                      icon={<Money20Regular />}
+                    >
+                      Pricing
+                    </Button>
+                  </CardFooter>
+                </Card>
+              </div>
+              <div className="col-12 col-md-6 col-lg-4 mx-0 px-2 pb-4">
+                <Card style={{ width: "100%" }}>
+                  <CardHeader
+                    header={<img src={openai_logo} style={{ maxHeight: 50 }} />}
+                  ></CardHeader>
+                  <p>
+                    OpenAi is a company that is working on artificial
+                    intelligence and machine learning.
+                  </p>
+                  <CardFooter>
+                    <Button
+                      as="a"
+                      href="/marketplace/openai"
+                      icon={<Info20Regular />}
+                    >
+                      Learn More
+                    </Button>
+                    <Button
+                      as="a"
+                      href="/marketplace/openai_pricing"
+                      icon={<Money20Regular />}
+                    >
+                      Pricing
+                    </Button>
+                  </CardFooter>
+                </Card>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
